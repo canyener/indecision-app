26dd69ddf2ebc393b6387a61f1d67437c271f59d
canyener <can.yener@hotmail.com.tr>
2019-11-21 23:50:00 +0300

    Adds an ordered list to template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,6 +14,20 @@ var template = React.createElement(
         'p',
         null,
         'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
     )
 );
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ var template = (
     <div>
         <h1>Indecision App</h1> 
         <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
     </div>
 );
 var appRoot = document.getElementById('app');
