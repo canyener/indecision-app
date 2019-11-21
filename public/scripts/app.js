dec51e553a23e3b64d2b606486f9a81ada6caffa
canyener <can.yener@hotmail.com.tr>
2019-11-21 23:44:02 +0300

    Formats template for readability

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,19 +3,18 @@
 //JSX - JavaScript XML
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 //JSX - JavaScript XML
 
-var template = <div><h1>Indecision App</h1> <p>This is some info</p></div>;
+var template = ( 
+    <div>
+        <h1>Indecision App</h1> 
+        <p>This is some info</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
