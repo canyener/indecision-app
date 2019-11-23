dc05f6d3399e7a429d2894ca4b7eee450a55cf3c
canyener <can.yener@hotmail.com.tr>
2019-11-23 23:22:00 +0300

    Adds examples for both regular and expression short handed syntax for arrow functions

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,19 @@
+// const square = function (x) {
+//     return x * x;
+// };
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(8));
+
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+// }
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+
+console.log(getFirstName('Can Yener'));
