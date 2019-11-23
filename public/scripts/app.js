9fad7a1da3da7812c028e778ddc682ffaabe5654
canyener <can.yener@hotmail.com.tr>
2019-11-23 23:04:55 +0300

    Adds example for block scoping

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,3 +14,14 @@ var nameConst = 'Osman';
 //nameConst = 'Ömür'; -> const is read-only!
 //const nameConst = 'Ömür';  -> Duplicate declaration!
 console.log('nameConst', nameConst);
+
+//Block Scoping
+
+var fullName = 'Can Yener';
+
+if (fullName) {
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName); //We can access if firstName is 'var', CANNOT access if it is 'let' or 'const'
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -12,3 +12,15 @@ const nameConst = 'Osman';
 //nameConst = 'Ömür'; -> const is read-only!
 //const nameConst = 'Ömür';  -> Duplicate declaration!
 console.log('nameConst', nameConst);
+
+
+//Block Scoping
+
+var fullName = 'Can Yener';
+
+if (fullName) {
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName);  //We can access if firstName is 'var', CANNOT access if it is 'let' or 'const'
