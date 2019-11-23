6e9b2d5906b54fe978471d574cbafff51554acf4
canyener <can.yener@hotmail.com.tr>
2019-11-23 22:56:42 +0300

    Adds examples using var-let-const keywords

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,16 +1,14 @@
-'use strict';
-
 var nameVar = 'Can';
 var nameVar = 'Cem';
 
 console.log('nameVar', nameVar);
 
-var nameLet = 'Ali';
+let nameLet = 'Ali';
 nameLet = 'Veli'; //valid
 //let nameLet = 'Veli'; -> Duplicate declaration!
 console.log('nameLet', nameLet);
 
-var nameConst = 'Osman';
+const nameConst = 'Osman';
 //nameConst = 'Ömür'; -> const is read-only!
 //const nameConst = 'Ömür';  -> Duplicate declaration!
 console.log('nameConst', nameConst);
