055c1ed0f42aadc69cb493e9703f9dd1d9144ec3
canyener <can.yener@hotmail.com.tr>
2019-11-24 16:00:06 +0300

    Moves counter example to its own seperate file

--- src/app.js
+++ src/app.js
@@ -18,35 +18,5 @@ const template = (
     </div>
 );
 
-let count = 0 ;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
