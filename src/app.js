f0a0fcbd3b04e0e01fbad2170a4af85772e84603
canyener <can.yener@hotmail.com.tr>
2019-11-24 22:15:34 +0300

    Creates the IndecisionApp parent component and renders it

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Action/>
+                <Options/>
+                <AddOption/>
+            </div>
+        )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -35,13 +48,5 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header/>
-        <Action/>
-        <Options/>
-        <AddOption/>
-    </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
