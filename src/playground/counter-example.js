53426ce2647600edf1b998d3990b56838c05c80b
canyener <can.yener@hotmail.com.tr>
2019-11-29 01:12:10 +0300

    Comments old code

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,32 +1,32 @@
-let count = 0 ;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
+// let count = 0 ;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// };
 
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// };
 
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
 
-const appRoot = document.getElementById('app');
+// const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>Reset</button>
+//         </div>
+//     );
 
-    ReactDOM.render(templateTwo, appRoot);
-};
+//     ReactDOM.render(templateTwo, appRoot);
+// };
 
-renderCounterApp();
+// renderCounterApp();
