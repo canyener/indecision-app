d4029b4f3e3c56a661bb9121efc01a2b45669129
canyener <can.yener@hotmail.com.tr>
2019-11-29 01:41:37 +0300

    Sets indentations to 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,81 +9,81 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Counter = function (_React$Component) {
-    _inherits(Counter, _React$Component);
+	_inherits(Counter, _React$Component);
 
-    function Counter(props) {
-        _classCallCheck(this, Counter);
+	function Counter(props) {
+		_classCallCheck(this, Counter);
 
-        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+		var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
 
-        _this.handleAddOne = _this.handleAddOne.bind(_this);
-        _this.handleMinusOne = _this.handleMinusOne.bind(_this);
-        _this.handleReset = _this.handleReset.bind(_this);
-        _this.state = {
-            count: 0
-        };
-        return _this;
-    }
+		_this.handleAddOne = _this.handleAddOne.bind(_this);
+		_this.handleMinusOne = _this.handleMinusOne.bind(_this);
+		_this.handleReset = _this.handleReset.bind(_this);
+		_this.state = {
+			count: 0
+		};
+		return _this;
+	}
 
-    _createClass(Counter, [{
-        key: 'handleAddOne',
-        value: function handleAddOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count + 1
-                };
-            });
-        }
-    }, {
-        key: 'handleMinusOne',
-        value: function handleMinusOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count - 1
-                };
-            });
-        }
-    }, {
-        key: 'handleReset',
-        value: function handleReset() {
-            this.setState(function () {
-                return {
-                    count: 0
-                };
-            });
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                    'h1',
-                    null,
-                    'Count: ',
-                    this.state.count
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleAddOne },
-                    '+1'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleMinusOne },
-                    '-1'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleReset },
-                    'Reset'
-                )
-            );
-        }
-    }]);
+	_createClass(Counter, [{
+		key: 'handleAddOne',
+		value: function handleAddOne() {
+			this.setState(function (prevState) {
+				return {
+					count: prevState.count + 1
+				};
+			});
+		}
+	}, {
+		key: 'handleMinusOne',
+		value: function handleMinusOne() {
+			this.setState(function (prevState) {
+				return {
+					count: prevState.count - 1
+				};
+			});
+		}
+	}, {
+		key: 'handleReset',
+		value: function handleReset() {
+			this.setState(function () {
+				return {
+					count: 0
+				};
+			});
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Count: ',
+					this.state.count
+				),
+				React.createElement(
+					'button',
+					{ onClick: this.handleAddOne },
+					'+1'
+				),
+				React.createElement(
+					'button',
+					{ onClick: this.handleMinusOne },
+					'-1'
+				),
+				React.createElement(
+					'button',
+					{ onClick: this.handleReset },
+					'Reset'
+				)
+			);
+		}
+	}]);
 
-    return Counter;
+	return Counter;
 }(React.Component);
 
 ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
