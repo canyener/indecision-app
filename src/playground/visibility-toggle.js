0d287d3e35f5e37ec58c4d0a4ba601ae717d9234
canyener <can.yener@hotmail.com.tr>
2019-11-29 22:14:22 +0300

    Comments all codes

--- src/playground/visibility-toggle.js
+++ src/playground/visibility-toggle.js
@@ -1,23 +1,23 @@
 
-let visibility = false
+// let visibility = false
 
-const onShowDetails = () => {
-    visibility = !visibility
-    render()
-}
+// const onShowDetails = () => {
+//     visibility = !visibility
+//     render()
+// }
 
-const appRoot = document.getElementById('app')
+// const appRoot = document.getElementById('app')
 
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={onShowDetails}>{!visibility ? 'Show Details' : 'Hide Details'}</button>
-            {visibility && <p>Here are the details</p>}
-        </div>
-    )
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={onShowDetails}>{!visibility ? 'Show Details' : 'Hide Details'}</button>
+//             {visibility && <p>Here are the details</p>}
+//         </div>
+//     )
 
-    ReactDOM.render(template, appRoot)
-}
+//     ReactDOM.render(template, appRoot)
+// }
 
-render()
+// render()
