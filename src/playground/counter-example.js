5f3d95176a5b0514c8b1d34d9d8beca74d234e43
canyener <can.yener@hotmail.com.tr>
2019-12-03 20:44:13 +0300

    Adds defaultProps for the counter example

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
 		this.handleMinusOne = this.handleMinusOne.bind(this)
 		this.handleReset = this.handleReset.bind(this)
 		this.state = {
-			count: 0
+			count: props.count
 		}
 	}
 	handleAddOne() {
@@ -39,6 +39,10 @@ class Counter extends React.Component {
 			</div>
 		)
 	}
+}
+
+Counter.defaultProps = {
+	count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
