89a07de4ba8007172a0253d9ce2e4b8cb2730af7
canyener <can.yener@hotmail.com.tr>
2019-12-05 22:33:11 +0300

    Adds a simple add function to demonstrate exporting multiple stuff from a file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //import './utils.js'
-import { square } from './utils.js'
+import { square, add } from './utils.js'
 
 console.log('app.js is running!')
-console.log(square(4))
+console.log('square function from utils.js: ', square(4))
+console.log('add function from utils.js: ', add(100, 23))
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,12 @@ console.log('utils.js is running')
 
 const square = x => x * x
 
+const add = (a, b) => a + b
+//Alternative way
+// export add = (a, b) => a + b
+
 //exports - default export - named exports
 export {
-  square
+  square,
+  add
 }
