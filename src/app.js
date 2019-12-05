4a3b818c773e53eacc37b5014e05183a0a86d70b
canyener <can.yener@hotmail.com.tr>
2019-12-06 00:01:14 +0300

    Renders a sample template to the screen using react npm packages

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-import validator from 'validator'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-console.log(validator.isEmail('test@gmail.com'))
+const template = React.createElement('p', {}, 'testing 123')
+
+ReactDOM.render(template, document.getElementById('app'))
