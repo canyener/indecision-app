013b3cd54774add3ca8face750e02fd9d7caf596
canyener <can.yener@hotmail.com.tr>
2019-12-06 01:19:15 +0300

    Moves AddOption component to its own seperate file and exports it as default

--- src/app.js
+++ src/app.js
@@ -142,37 +142,6 @@ const Option = (props) => {
   )
 }
 
-class AddOption extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.state = {
-      error: undefined
-    }
-  }
-  handleAddOption(e){
-    e.preventDefault()
-    
-    const option = e.target.elements.option.value.trim()
-    const error = this.props.handleAddOption(option)
 
-    this.setState(() => ({ error }))
-
-    if(!error) {
-      e.target.elements.option.value = ''
-    }
-  }
-  render() {
-    return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
-          <button>Add Option</button>
-        </form>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
