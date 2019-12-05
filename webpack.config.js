1299eb91663fcbf66a3f9e29b23231f529449be5
canyener <can.yener@hotmail.com.tr>
2019-12-06 00:41:44 +0300

    Adds module rules for babel to webpack andadds babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  }, 
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
   }
 }
