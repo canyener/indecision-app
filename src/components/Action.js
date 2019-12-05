3037a69c40f4cb1e1e0b44515a1947cefb021968
canyener <can.yener@hotmail.com.tr>
2019-12-06 01:28:22 +0300

    Moves Action component into its own seperate file

---
+++ src/components/Action.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What should I do?
+      </button>
+    </div>
+  )
+}
+
+export default Action
