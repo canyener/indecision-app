9b884efabde13b306ce3d9a9cc7142e746c66903
canyener <can.yener@hotmail.com.tr>
2019-12-05 22:52:15 +0300

    Sets default export for person.js with the alternative export syntax

--- src/person.js
+++ src/person.js
@@ -1,7 +1,5 @@
-const isAdult = (age) => age >= 18
-const canDrink = (age) => age >= 21
+export const isAdult = (age) => age >= 18
 
-export {
-  isAdult,
-  canDrink
-}
+export const canDrink = (age) => age >= 21
+
+export default (age) => age >= 65
