7994be3a4643b78ad4b83aa058ff2708394eeaff
canyener <can.yener@hotmail.com.tr>
2019-12-05 22:26:19 +0300

    Creates utils.js, adds a simple function to use es6 import and named exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+//import './utils.js'
+import { square } from './utils.js'
+
 console.log('app.js is running!')
+console.log(square(4))
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is running')
+
+const square = x => x * x
+
+//exports - default export - named exports
+export {
+  square
+}
