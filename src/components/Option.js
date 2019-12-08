33ed950be68ec23e039db68232dd5d63d99eadbe
canyener <can.yener@hotmail.com.tr>
2019-12-08 16:21:11 +0300

    Refactors all stateless functional components by implicitly returning the jsx

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-const Option = (props) => {
-  return (
-    <div>
-      {props.optionText}
-      <button 
-        onClick={(e) => {
-          props.handleDeleteOption(props.optionText)
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  )
-}
+const Option = (props) => (
+  <div>
+    {props.optionText}
+    <button 
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText)
+      }}
+    >
+      Remove
+    </button>
+  </div>
+)
 
 export default Option
