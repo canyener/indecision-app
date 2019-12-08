eeeb4410d868740189a58c38c62a89ffefb60651
canyener <can.yener@hotmail.com.tr>
2019-12-08 14:18:23 +0300

    Adds devServer configuration

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,8 @@ module.exports = {
       exclude: /node_modules/
     }]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 }
