9eaccd889de50dc52fe2e05f419f5e29b114c9f2
canyener <can.yener@hotmail.com.tr>
2019-12-12 22:03:11 +0300

    Adds styles for buttons

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Action = (props) => (
   <div>
-    <button 
+    <button
+      className="big-button" 
       onClick={props.handlePick}
       disabled={!props.hasOptions}
     >
--- src/components/Options.js
+++ src/components/Options.js
@@ -5,7 +5,12 @@ import Option from './Option'
 const Options = (props) => (
   <div>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <button 
+      className="button button--link"
+      onClick={props.handleDeleteOptions}
+    >
+      Remove All
+    </button>
     {
       props.options.map(option =>  (
         <Option 
