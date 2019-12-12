45fcc3cd44fd169e0ee7f3a2919cadf7907c5037
canyener <can.yener@hotmail.com.tr>
2019-12-13 01:15:18 +0300

    Styles modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
     contentLabel="Selected Option"
     ariaHideApp={false}
     onRequestClose = {props.handleClearSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 )
 
