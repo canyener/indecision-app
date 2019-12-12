b69101e8d87319755c0c986704855ae957e0302e
canyener <can.yener@hotmail.com.tr>
2019-12-12 20:36:21 +0300

    Tweaks css rule to scss

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/
     }, {
-      test: /\.css$/,
+      test: /\.scss$/,
       use: [
         'style-loader',
         'css-loader'
