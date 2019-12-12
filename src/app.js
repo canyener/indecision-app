4def6208674e08d81d40b5700e72d2c46de0eb00
canyener <can.yener@hotmail.com.tr>
2019-12-12 20:33:13 +0300

    Adds rule for css and style loaders to webpack and imports example css to app.js to test it

--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 
 import IndecisionApp from './components/IndecisionApp'
 
+import './styles/styles.css'
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
