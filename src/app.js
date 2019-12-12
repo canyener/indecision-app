d42b8f5850b3e7597e706dba3311fa70f7866f47
canyener <can.yener@hotmail.com.tr>
2019-12-12 21:12:31 +0300

    Adds basic style for header component and adds normalize.css to reset default browser css

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 import IndecisionApp from './components/IndecisionApp'
 
+import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
