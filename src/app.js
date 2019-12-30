1e351f85b41fe6c36141a8f3f9f1e5357614f7c1
canyener <can.yener@hotmail.com.tr>
2019-12-31 01:40:25 +0300

    Removes normalize.css npm package

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 
 import IndecisionApp from './components/IndecisionApp'
 
-import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
